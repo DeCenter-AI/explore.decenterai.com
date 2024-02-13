2fc82357034803949675e6bc3ea6422b8ec886f0
techyNonso <williamikeji@gmail.com>
2024-02-13 09:58:27 +0100

    changed to company emai

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-02-12'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-02-13'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image'
-import { AppMetaData } from '@config/app'
 
-export const metadata = AppMetaData
 export default function Home() {
   return (
     <main className="bg-primary_13">
-      Home
+      Home now
     </main>
   )
 }
