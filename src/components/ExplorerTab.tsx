bf7e0a63eeaa8b8c34f880a5ebb2fc389aa2d601
sheriff Oladimeji <dimejiademola5@gmail.com>
2024-02-14 14:27:32 +0100

    fix : upadate tabs componnet

--- src/components/ExplorerTab.tsx
+++ src/components/ExplorerTab.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
 import Search from './Search'
 
 const ExplorerTab = () => {
   return (
-    <div className="bg-[#19191926] text-center py-6">
+    <div className="bg-[#19191926] text-center py-6 flex flex-col items-center gap-6">
       <Search />
-      <Tabs defaultValue="favorite" className="w-[400px] ">
-        <TabsList className="bg-card text-[#C1C1C1]   border-[#232323]">
-          <TabsTrigger value="favorite">Favorite</TabsTrigger>
-          <TabsTrigger value="password">All</TabsTrigger>
+      <Tabs defaultValue="account" className="">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
         </TabsList>
-        <TabsContent value="favorite">Make changes to your account here.</TabsContent>
-        <TabsContent value="all">Change your password here.</TabsContent>
+        <TabsContent value="account">Make changes to your account here.</TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
       </Tabs>
     </div>
   )
