a3cc1c7d5f4929522a07e5e5049c2a030642a93c
Raji Roqeeb Olalekan <105174934+Codebox124@users.noreply.github.com>
2024-02-14 13:54:21 +0100

    Items/nft (#6)
    * created the nft_item component
    
    * created the nft_item component
    
    * created the nft_item component
    
    * adjusted
    
    ---------
    
    Co-authored-by: techyNonso <williamikeji@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 
 //import { AppMetaData } from '@config/app'
 import Footer from '@/components/ui/footer'
+import NftCard from '@/components/ui/nft_item'
 
 // export const metadata = AppMetaData
 export default function Home() {
   return (
     <>
+      <NftCard />
       <Footer />
     </>
   )
