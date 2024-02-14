fd2a792ef39064b2d784c80b9399adf90703a97d
sheriff Oladimeji <dimejiademola5@gmail.com>
2024-02-14 13:59:30 +0100

    feat: update search component

--- src/components/ExplorerTab.tsx
+++ src/components/ExplorerTab.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import Search from './Search'
 
 const ExplorerTab = () => {
   return (
-    <Tabs defaultValue="favorite" className="w-[400px] ">
-      <TabsList className="bg-card text-[#C1C1C1]   border-[#232323]">
-        <TabsTrigger value="favorite">Favorite</TabsTrigger>
-        <TabsTrigger value="password">All</TabsTrigger>
-      </TabsList>
-      <TabsContent value="favorite">Make changes to your account here.</TabsContent>
-      <TabsContent value="all">Change your password here.</TabsContent>
-    </Tabs>
+    <div className="bg-[#19191926] text-center py-6">
+      <Search />
+      <Tabs defaultValue="favorite" className="w-[400px] ">
+        <TabsList className="bg-card text-[#C1C1C1]   border-[#232323]">
+          <TabsTrigger value="favorite">Favorite</TabsTrigger>
+          <TabsTrigger value="password">All</TabsTrigger>
+        </TabsList>
+        <TabsContent value="favorite">Make changes to your account here.</TabsContent>
+        <TabsContent value="all">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
   )
 }
 
---
+++ src/components/Search.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { IoSearchOutline } from 'react-icons/io5'
+
+const Search = () => {
+  return (
+    <div className="flex items-center rounded-full bg-primary_9 py-2 px-4  w-max sm:pr-8  mx-auto">
+      <IoSearchOutline className="mr-2 text-primary_7" size={30} />
+      <input
+        type="search"
+        className="w-full bg-transparent placeholder:text-primary_7 focus:outline-none focus:border-none"
+        placeholder="Search"
+      />
+    </div>
+  )
+}
+
+export default Search
